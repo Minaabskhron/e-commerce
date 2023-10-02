262d12ec8cf0cc4f53c5ddcd922f83d0c03cb37d
minaabskhron <minaabskhron@gmail.com>
2023-10-02 21:03:38 +0300

    sixth commit protectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,34 @@ import Products from './Components/Products/Products';
 import Register from './Components/Register/Register';
 import Login from './Components/Login/Login';
 import AuthContextProvider from './context/authentication';
+import NotFound from './Components/NotFound/NotFound';
+import ProtectedRoute from './Components/ProtectedRoute/ProtectedRoute';
 
 
 function App() {
   let routers = createHashRouter([{
     path:"",element:<Layout/>,children:[
-    {index:true,element:<Home/>  },
-    {path:"brands",element:<Brands /> },
-    {path:"Categories",element:<Catogries/> },
-    {path:"products",element:<Products/> },
+    {index:true,element:<ProtectedRoute>
+      <Home/> 
+    </ProtectedRoute>
+     },
+
+    {path:"brands",element:<ProtectedRoute>
+    <Brands /> 
+    </ProtectedRoute>},
+
+    {path:"Categories",element:<ProtectedRoute>
+    <Catogries/>
+    </ProtectedRoute>
+    },
+
+    {path:"products",element:<ProtectedRoute>
+    <Products/> 
+    </ProtectedRoute>},
+
     {path:"Register",element:<Register/> },
-    {path:"login", element: <Login/>}
+    {path:"login", element: <Login/>},
+    {path:"*", element: <NotFound/>}
   ]}])
   
   return <>
